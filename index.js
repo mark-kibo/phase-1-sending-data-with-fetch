5b9b8c106efbb6df1a798737a6b36bc069276f5a
mark-kibo <kibochamark@gmail.com>
2023-07-26 13:12:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name="mark", email="kibochamark@gmai.com"){
+    const configurationObject={
+        method:"POST", 
+        headers:{
+            "Content-Type":"application/json",
+            "accept": "application/json"
+        },
+        body:JSON.stringify({
+            "name":name,
+            "email":email
+        })
+    }
+
+    fetch("http://localhost:3000/users", configurationObject)
+    .then(res => res.json())
+    .then(function(object){
+        // console.log(object)
+        
+        let p = document.createElement('p')
+        p.innerHTML=object.id
+        let body =document.querySelector("body")
+        body.appendChild(p)
+    }).catch(e=>console.log(e))
+}
+
+
+// submitData("mark", "kibochamark@gmail.com")
